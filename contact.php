edb603678d4fa3f94e331dd16100ce5d0d9f944d
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-11-24 21:22:39 +0530

    Updated Contacts

--- contact.php
+++ contact.php
@@ -24,50 +24,69 @@
 
 <body>
 
-  <?php include("Includes/header.php"); ?>
+    <?php include("Includes/header.php"); ?>
 
-  <div style="background: #004aad;height: 6.6rem;"></div>
+    <div style="background: #004aad;height: 6.6rem;"></div>
 
-  <div class="con-container">
-    <div class="content">
-      <div class="left-side">
-        <div class="phone details">
-          <i class="fas fa-phone-alt"></i>
-          <div class="topic">Phone</div>
-          <div class="text-one">+91 9310519210</div>
-          <div class="text-two">+91 8076654886</div>
-          <div class="text-one">+91 8766393891</div>
+    
+    <div class="con-container">
+
+        <div class="content">
+
+            <div class="left-side">
+
+                <div class="phone details">
+
+                    <i class="fas fa-phone-alt"></i>
+                    <div class="topic">Phone</div>
+                    <a href="tel:+919310519210" class="text-one">+91 9310519210</a><br>
+                    <a href="tel:+918076654886" class="text-two">+91 8076654886</a><br>
+                    <a href="tel:+918766393891" class="text-one">+91 8766393891</a>
+
+                </div>
+
+                <div class="email details">
+
+                    <i class="fas fa-envelope"></i>
+                    <div class="topic">Email</div>
+                    <a href="mailto:simran033btcsai20@gmail.com" class="text-one">simran033btcsai20@gmail.com</a>
+                    <a href="mailto:simran065btcsai20@igdtuw.ac.in" class="text-two">simran065btcsai20@igdtuw.ac.in</a>
+                    <a href="mailto:srishti060btcsai20@igdtuw.ac.in" class="text-two">srishti060btcsai20@igdtuw.ac.in</a>
+                </div>
+
+            </div>
+
+            <div class="right-side">
+
+                <div class="topic-text">Send us a message</div>
+
+                <p>If you have any type of query or suggestion related to our services, you can send us message from here. It's our pleasure to help you.</p>
+                
+                <form action="#">
+
+                    <div class="input-box">
+                        <input type="text" placeholder="Enter your name">
+                    </div>
+
+                    <div class="input-box">
+                        <input type="text" placeholder="Enter your email">
+                    </div>
+
+                    <div class="input-box message-box">
+                        <textarea placeholder="Enter your message"></textarea>
+                    </div>
+
+                    <div class="button">
+                        <input type="button" value="Send Now">
+                    </div>
+
+                </form>
+
+            </div>
 
         </div>
-        <div class="email details">
-          <i class="fas fa-envelope"></i>
-          <div class="topic">Email</div>
-          <div class="text-one">simran033btcsai20@gmail.com</div>
-          <div class="text-two">simran065btcsai20@igdtuw.ac.in</div>
-          <div class="text-two">srishti060btcsai20@igdtuw.ac.in</div>
-        </div>
-      </div>
-      <div class="right-side">
-        <div class="topic-text">Send us a message</div>
-        <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from
-          here. It's my pleasure to help you.</p>
-        <form action="#">
-          <div class="input-box">
-            <input type="text" placeholder="Enter your name">
-          </div>
-          <div class="input-box">
-            <input type="text" placeholder="Enter your email">
-          </div>
-          <div class="input-box message-box">
-            <textarea placeholder="Enter your message"></textarea>
-          </div>
-          <div class="button">
-            <input type="button" value="Send Now">
-          </div>
-        </form>
-      </div>
+    
     </div>
-</div>
 
     <?php include("Includes/footer.php"); ?>
 
