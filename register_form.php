5bfb44b1fdf44339f295c69119c879a5fa413d0f
Srishtikumari2002 <77767357+Srishtikumari2002@users.noreply.github.com>
2021-11-25 15:16:21 +0530

    Add files via upload

---
+++ register_form.php
@@ -0,0 +1,35 @@
+<html>
+
+<title> REGISTRATION FORM </title>
+
+<body>
+    <div class="abc">
+        <div class="row">
+            <h1> REGISTRATION FORM </h1>
+        </div>
+        <div class="main_form">
+            <div class="col1">
+                <link rel="stylesheet" href="register_form.css">
+                <form method="post" action="register_submit.php">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" />
+                    <label for="email">Email</label>
+                    <input type="email" name="email"/>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" /> 
+                    <input type="submit" name="submit" value="Submit" />
+                </form>
+            </div>
+            <div class="col2">
+              <p>
+                <span>WELCOME</span><br/>
+                <span> Glad to see you!</span><br/>
+                <span>create account</span>
+              </p>
+             </div>
+        </div>
+        
+    </div>
+</body>
+
+</html>
