bf741703dc06396159f2e864521d193b10c75cdc
Srishtikumari2002 <77767357+Srishtikumari2002@users.noreply.github.com>
2021-11-28 22:08:40 +0530

    Update mysql_details.php

--- Background/mysql_details.php
+++ Background/mysql_details.php
@@ -4,13 +4,9 @@
 // and comment the uncommented part before
 // running
 
-// $db_hostname="localhost";
-// $db_username="root";
-// $db_password="";
-// $db_name="airbar";
+$db_hostname="localhost";
+$db_username="username";
+$db_password="password";
+$db_name="database name";
 
-$db_hostname="ec2-54-157-16-125.compute-1.amazonaws.com:5432";
-$db_username="setdcgltmmjxoc";
-$db_password="89c5fca1eeb42919b2e3d5cb09b37aff9e1d9925a17aa3396a66ee66b35e18de";
-$db_name="dbao78lrhj2517";
 ?>
