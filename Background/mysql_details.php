9443e9ec8348c84cd7453b12a8658def489a19c1
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-11-30 18:14:28 +0530

    Update

--- Background/mysql_details.php
+++ Background/mysql_details.php
@@ -3,8 +3,4 @@ $db_hostname=getenv('host');
 $db_username=getenv('user');
 $db_password=getenv('psd');
 $db_name=getenv('db');
-// $db_hostname="sql6.freesqldatabase.com";
-// $db_username="sql6455248";
-// $db_password="ZHSZaH356F";
-// $db_name="sql6455248";
 ?>
