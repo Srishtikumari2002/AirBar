e4c1cc6aa73e9927094fb430759318bc7cbf9f63
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-11-30 18:01:08 +0530

    Update mysql_details

--- Background/mysql_details.php
+++ Background/mysql_details.php
@@ -1,6 +1,10 @@
 <?php
-$db_hostname="sql6.freesqldatabase.com";
-$db_username="sql6455248";
-$db_password="ZHSZaH356F";
-$db_name="sql6455248";
+$db_hostname=get_env('host');
+$db_username=get_env('user');
+$db_password=get_env('psd');
+$db_name=get_env('db');
+// $db_hostname="sql6.freesqldatabase.com";
+// $db_username="sql6455248";
+// $db_password="ZHSZaH356F";
+// $db_name="sql6455248";
 ?>
