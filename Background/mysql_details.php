748caae1b645d98acb64dfd39f7fb537ad88e053
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-11-30 15:01:02 +0530

    Updated

--- Background/mysql_details.php
+++ Background/mysql_details.php
@@ -1,16 +1,6 @@
 <?php
-
-// Uncomment the comment part of the code 
-// and comment the uncommented part before
-// running
-
-// $db_hostname="localhost";
-// $db_username="root";
-// $db_password="";
-// $db_name="airbar";
-
-$db_hostname="ec2-54-157-16-125.compute-1.amazonaws.com:5432";
-$db_username="setdcgltmmjxoc";
-$db_password="89c5fca1eeb42919b2e3d5cb09b37aff9e1d9925a17aa3396a66ee66b35e18de";
-$db_name="dbao78lrhj2517";
+$db_hostname="sql312.epizy.com";
+$db_username="epiz_30483429";
+$db_password="xqDvZ1tmmy";
+$db_name="epiz_30483429_airbar";
 ?>
--- airbar.sql
+++ airbar.sql
@@ -4,7 +4,7 @@ use airbar;
 
 create table users (
     id int PRIMARY KEY AUTO_INCREMENT,
-    name varchar(255) not null;
+    name varchar(255) not null,
 email varchar(255) not null unique,
 psd varchar(255) not null
 );
