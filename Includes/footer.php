4ea4dfab90fb04827ad9143e519848da68098fdc
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-11-30 22:43:57 +0530

    Updated Footer

--- Includes/footer.php
+++ Includes/footer.php
@@ -13,74 +13,57 @@
     <link rel="stylesheet" href="Css/footer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
-    <!-- importing third party javascript -->
-
-
-    <!-- custom javascript files -->
-
-    <!-- bootstrap  -->
-
-
 </head>
 
 <body>
     <footer>
-        <div class="article">
+        <div class="content">
             <div class="left box">
                 <div class="upper">
                     <div class="topic">About us</div>
-                    <p>We at AirBar aim to provide user with fast,simple and secure airline management system.</p>
+                      <p>CodingLab is a channel where you can learn HTML,
+                      CSS, and also JavaScript along with creative CSS Animations and Effects.</p>
                 </div>
                 <div class="lower">
                     <div class="topic">Contact us</div>
                     <div class="phone">
-                        <a href="#"><i class="fas fa-phone-volume"></i>8076654886</a>
+                        <a href="#"><i class="fas fa-phone-volume"></i>+007 9089 6767</a>
                     </div>
                     <div class="email">
-                        <a href="#"><i class="fas fa-envelope"></i>simran65btcsai@igdtuw.ac.in</a>
-                    </div>
-                    <div class="phone">
-                        <a href="#"><i class="fas fa-phone-volume"></i>8076654886</a>
-                    </div>
-                    <div class="email">
-                        <a href="#"><i class="fas fa-envelope"></i>simran33btcsai@igdtuw.ac.in</a>
-                    </div>
-                    <div class="phone">
-                        <a href="#"><i class="fas fa-phone-volume"></i>8076654886</a>
-                    </div>
-                    <div class="email">
-                        <a href="#"><i class="fas fa-envelope"></i>simran13059@gmail.com</a>
+                        <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
                     </div>
                 </div>
             </div>
             <div class="middle box">
                 <div class="topic">Our Services</div>
-                <div><a href="#">Software source aPI, Development</a></div>
-                <div><a href="#">Software dev Source code, Reasearch</a></div>
-                <div><a href="#">Software User Interface Source code</a></div>
-                <div><a href="#">Laravel Development, Source code</a></div>
-                <div><a href="#">Mobile Application Source code</a></div>
-                <div><a href="#">Blogging & Earning</a></div>
+                <div><a href="#">Web Design, Development</a></div>
+                <div><a href="#">Web UX Design, Reasearch</a></div>
+                <div><a href="#">Web User Interface Design</a></div>
+                <div><a href="#">Theme Development, Design</a></div>
+                <div><a href="#">Mobile Application Design</a></div>
+                <div><a href="#">Wire raming & Prototyping</a></div>
             </div>
             <div class="right box">
                 <div class="topic">Subscribe us</div>
-                <form action="#">
-                    <input type="text" placeholder="Enter Your email address">
-                    <input type="submit" name="" value="Send">
-                    <div class="media-icons">
+                  <form action="#">
+                      <input type="text" placeholder="Enter email address">
+                      <input type="submit" name="" value="Send">
+                      <div class="media-icons">
                         <a href="#"><i class="fab fa-facebook-f"></i></a>
                         <a href="#"><i class="fab fa-instagram"></i></a>
                         <a href="#"><i class="fab fa-twitter"></i></a>
                         <a href="#"><i class="fab fa-youtube"></i></a>
                         <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </form>
+                      </div>
+                  </form>
             </div>
         </div>
-        <div class="bottom">
-            <p>Copyright © 2021 <a href="#">AirBar</a> All rights reserved</p>
-        </div>
+            <div class="bottom">
+                <p>Copyright © 2020 <a href="#">AirBar</a> All rights reserved</p>
+            </div>
+        
     </footer>
+
 </body>
 
 </html>
