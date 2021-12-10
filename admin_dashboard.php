32af52c455737ca652eb10de144154cf9e0a5621
Srishtikumari2002 <tiwarisrishti25@gmail.com>
2021-12-10 22:26:42 +0530

    .

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -20,8 +20,8 @@
     <link rel="icon" href="Images/favicon.png" type="image/png">
 
     <!-- javascript files -->
-	<script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script defer src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 
